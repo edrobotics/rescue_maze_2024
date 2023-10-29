d565e0fdd016c32df13318e45b1bb7dd4646228b
apollen85 <123020764+apollen85@users.noreply.github.com>
2023-10-29 12:54:57 +0100

    fixed motor controller direction and added debugging

--- motor-control/include/MotorController.h
+++ motor-control/include/MotorController.h
@@ -16,12 +16,15 @@ class MotorController
     bool init();
     void update(); // Read encoder positions and set new PID loop value
     void setSpeed(double speed); // Sets the speed in rpm
-    void setPWM(int pwmSpeed); // Sets the PWM output. Clamps and returns false if too great.
+    void setPWM(int pwmSpeed); // Sets the PWM output. Clamps if too great.
 
     void startDistanceMeasure(); // Begin distance measuring
     // Maybe replace these with passing pointers in the beginning and then updating the values through pointers?
     double getDistance(); // Returns the distance turned on output shaft since beginning distance measuring. Measured in rounds. Maybe int where every step is part of full revolution?
-    double getOtputSpeed(); // Returns the current speed of the output shaft, measuren in rpm.
+    double getOutputSpeed(); // Returns the current speed of the output shaft, measuren in rpm.
+    bool isReversed {false}; // If the direction of the motor is inverted. Should this apply to the encoderPosition as well?
+
+    void printValues();
 
 
     private:
@@ -44,17 +47,16 @@ class MotorController
     // Motor controlling
     int pwmPin;
     int dirPin;
-    bool isReversed {false}; // If the direction of the motor is inverted. Should this apply to the encoderPosition as well?
     double outputSpeed {0}; // Wanted output speed in rpm
     float motorSpeed {0}; // Wanted motor speed in rpm
-    float pwmCorrection;
+    float speedCorrection;
 
     // PID
     #warning untuned constants
-    float Kp {0};
+    float Kp {0.04};
     float Ki {0};
     float Kd {0};
-    QuickPID pid {&curMotorSpeed, &pwmCorrection, &motorSpeed, Kp, Ki, Kd, QuickPID::pMode::pOnError, QuickPID::dMode::dOnMeas, QuickPID::iAwMode::iAwCondition, QuickPID::Action::reverse};
+    QuickPID pid {&curMotorSpeed, &speedCorrection, &motorSpeed, Kp, Ki, Kd, QuickPID::pMode::pOnError, QuickPID::dMode::dOnMeas, QuickPID::iAwMode::iAwCondition, QuickPID::Action::direct};
     
 
 };
