097783c17771b8b95d60286773d7fb67b09d30f5
MrKussen <123412590+MrKussen@users.noreply.github.com>
2023-11-23 18:04:18 +0100

    Changed command line, update gitignore

--- lidar/compile.sh
+++ lidar/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # change pls
 echo "Fix this script idiot"
-g++ -g ./src/*.cpp -I./include/ -I/usr/include/opencv4/ -o ld_test -D LINUX -L/home/markfri/code/opencv/build/lib/ -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc #-LC:/msys64/ucrt64/lib/ -L./lib/ -lsetupapi -lopencv_core470 -lopencv_imgcodecs470 -lopencv_imgproc470
+g++ -g ./src/*.cpp -I./include/ -o ld_test -D LINUX `pkg-config --cflags --libs opencv4` #-I/usr/include/opencv4/ -L/home/markfri/code/opencv/build/lib/ -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc #-LC:/msys64/ucrt64/lib/ -L./lib/ -lsetupapi -lopencv_core470 -lopencv_imgcodecs470 -lopencv_imgproc470
 
 echo "compiled successfully"
