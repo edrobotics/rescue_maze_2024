b6aced75ad0294d25ba233936a67f1ac47f61a38
apollen85 <123020764+apollen85@users.noreply.github.com>
2023-12-21 12:34:05 +0100

    fixed build script again (better debugging)

--- mazenav/build.sh
+++ mazenav/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -g -std=gnu++17 ./src/*.cpp ./src/*/*.cpp -I./include/ -o ./build/mazenav -O3 #use O3 or O2 if not working (previously Ofast)
+g++ -g -std=gnu++17 ./src/*.cpp ./src/*/*.cpp -I./include/ -o ./build/mazenav -Og #use O3 or O2 if not working (previously Ofast)
 chmod +x ./build/mazenav
 
 echo "compiled successfully (maybe)"
