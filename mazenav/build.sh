7a7f6a067c26d60a15c3f1c87242824e7ebdde09
MrKussen <123412590+MrKussen@users.noreply.github.com>
2024-04-08 09:10:59 +0200

    Build file now creates build directory

--- mazenav/build.sh
+++ mazenav/build.sh
@@ -21,6 +21,7 @@ else
     exit 1
 fi
 
+mkdir ./build
 
 g++ -g -std=gnu++17 -pedantic -Wall -Wextra `find src -iregex ".*\.cpp"` `find ../shared_lib -iregex ".*\.cpp"` -I./include/ -I../shared_lib/ -I/usr/local/boost_1_82_0/ -o ./build/mazenav -Og $envDefine #use O3 or O2 if not working (previously Ofast)
 chmod +x ./build/mazenav
