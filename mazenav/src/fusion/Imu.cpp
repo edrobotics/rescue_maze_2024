d6500c7dc53846c3c32cb293fa4cbc0f00894757
apollen85 <123020764+apollen85@users.noreply.github.com>
2024-04-11 23:11:56 +0200

    fixed IMU gimbal lock leading to weird angles

--- mazenav/src/fusion/Imu.cpp
+++ mazenav/src/fusion/Imu.cpp
@@ -47,37 +47,109 @@ void Imu::printVals(bool newLine)
     mtx_general.unlock();
 }
 
+void threeaxisrot(double r11, double r12, double r21, double r31, double r32, double res[]){
+    res[0] = atan2( r31, r32 );
+    res[1] = asin ( r21 );
+    res[2] = atan2( r11, r12 );
+}
 // You should probably not touch this function ever again, unless you know for certain there is a bug here. This is dark magic... very dark.
 Imu::EulerAngle Imu::quaternionToEuler(Quaternion q)
 {
-    // YZX rotation sequence is used
 
 
-    EulerAngle localData {};
+    if (true)
+    {
+        // NEW, WORKING CODE
+        // Test with rotation order (robot) XYZ, which should be IMU rotation order YXZ
+        EulerAngle localData {};
 
-    // Code taken/inspired from here: https://web.archive.org/web/20140824191842/http://bediyap.com/programming/convert-quaternion-to-euler-rotations/
-    // Note: I switched z and y as they seemed to be swapped. Then it worked perfectly. But why?
+        double r31 {2*(q.i*q.j + q.real*q.k)};
+        double r32 {q.real*q.real - q.i*q.i + q.j*q.j - q.k*q.k};
+        localData.x = atan2(r31, r32);
 
-    double r31 = -2*(q.j*q.k - q.real*q.i);
-    double r32 = q.real*q.real - q.i*q.i + q.j*q.j - q.k*q.k;
-    localData.x = atan2(r31, r32);
+        double r21 {-2*(q.j*q.k - q.real*q.i)};
+        localData.y = asin(r21);
+        
+        double r11 {2*(q.i*q.k + q.real*q.j)};
+        double r12 {q.real*q.real - q.i*q.i - q.j*q.j + q.k*q.k};
+        localData.z = atan2(r11, r12);
 
-    double r21 = 2*(q.i*q.j + q.real*q.k);
-    localData.z = asin(r21);
+        EulerAngle returnData {};
+        returnData.x = -localData.z;
+        returnData.y = localData.y;
+        returnData.z = localData.x;
 
-    double r11 = -2*(q.i*q.k - q.real*q.j);
-    double r12 = q.real*q.real + q.i*q.i - q.j*q.j - q.k*q.k;
-    localData.y = atan2(r11, r12);
+        // Verklighet => nuvarande
+        // z => y
+        // y => -x
+        // x => -z
 
-    EulerAngle returnData {};
+        return returnData;
+    }
 
-    // Change from sensor local coordinate system to global coordinate system conventions. New rotation order is XZY
-    returnData.x = -localData.y;
-    returnData.y = localData.x;
-    returnData.z = localData.z;
+    if (false)
+    {
+        // TEST
+        // YZX rotation sequence is used for the IMU => XZY for robot (?)
+
+        // Through testing: rotation order seems to be ZYX, but the axis are wrong. X and Z seem flipped
+        EulerAngle localData {};
+
+        double r31 {-2*(q.j*q.k - q.real*q.i)};
+        double r32 {q.real*q.real - q.i*q.i + q.j*q.j - q.k*q.k};
+        localData.x = atan2(r31, r32);
+
+        double r21 {2*(q.i*q.j + q.real*q.k)};
+        localData.y = asin(r21);
+
+        double r11 {-2*(q.i*q.k - q.real*q.j)};
+        double r12 {q.real*q.real + q.i*q.i - q.j*q.j - q.k*q.k};
+        localData.z = atan2(r11, r12);
+
+        EulerAngle returnData {};
+
+        // returnData.x = -localData.y;
+        // returnData.y = localData.x;
+        // returnData.z = localData.z;
+
+        return localData;
+
+    }
+
+    if (false)
+    {
+        // ORIGINAL CODE
+        
+        // YZX rotation sequence is used
+
+        EulerAngle localData {};
+
+        // Code taken/inspired from here: https://web.archive.org/web/20140824191842/http://bediyap.com/programming/convert-quaternion-to-euler-rotations/
+        // Note: I switched z and y as they seemed to be swapped. Then it worked perfectly. But why?
+
+        double r31 = -2*(q.j*q.k - q.real*q.i);
+        double r32 = q.real*q.real - q.i*q.i + q.j*q.j - q.k*q.k;
+        localData.x = atan2(r31, r32);
+
+        double r21 = 2*(q.i*q.j + q.real*q.k);
+        localData.z = asin(r21);
+
+        double r11 = -2*(q.i*q.k - q.real*q.j);
+        double r12 = q.real*q.real + q.i*q.i - q.j*q.j - q.k*q.k;
+        localData.y = atan2(r11, r12);
+
+        EulerAngle returnData {};
+
+        // Change from sensor local coordinate system to global coordinate system conventions. New rotation order is XZY
+        returnData.x = -localData.y;
+        returnData.y = localData.x;
+        returnData.z = localData.z;
 
 
-    return returnData;
+        return returnData;
+
+    }
+
 
 }
 
